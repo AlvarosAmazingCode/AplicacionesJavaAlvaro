ca4ee5fee13790b0be0b36299e2a9082289be9cf
AlvarosAmazingCode <crazycoquecode@gmail.com>
2016-04-14 14:41:46 +0200

    He añadido una mayuscula

--- EJs/src/condicionales/Ej1.java
+++ EJs/src/condicionales/Ej1.java
@@ -7,7 +7,7 @@ public class Ej1 {
 	public static void main(String[] args) {
 		int n = Integer.parseInt(JOptionPane.showInputDialog("dime un numero"));
 		if(n > -1){
-			System.out.println("es positivo"); 
+			System.out.println("Es positivo"); 
 		}else{
 			System.out.println("Es negativo");
 		}
