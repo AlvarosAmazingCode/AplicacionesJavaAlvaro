2bb3639d32cf046bc29a24afcebc9381f110a28a
Salakomg <salakorte@outlook.es>
2016-05-05 19:44:05 +0200

    bola 2.0

--- Ball/src/bal/Main.java
+++ Ball/src/bal/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		Ventana v = new Ventana();
 		v.setVisible(true);
-		v.setSize(2000, 1200);
+		v.setSize(2000, 1000);
 		v.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		MyPanel panel = new MyPanel();
 		v.setContentPane(panel);
--- Ball/src/bal/MyPanel.java
+++ Ball/src/bal/MyPanel.java
@@ -5,16 +5,59 @@ import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
+@SuppressWarnings("serial")
 public class MyPanel extends JPanel{
-	float y = 600;
-	float x = 0;
-		
+	float y = 700f;
+	float x = 600f;
+	
+	float vy = 0.5f;
+	float vx = 0.2f;
+	
+	float anchoBola = (int)(Math.random()*150);
+	float altoBola = (int)(Math.random()*150);
+	Color c = Color.BLUE ;
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
-		g.fillOval((int)x, (int) y, 100, 100);
-		x = x + 0.6f;
-		y = y - 0.6f;
-		repaint();
+		setBackground(Color.GRAY);
+			g.setColor(c);
+			g.fillOval((int)x, (int) y, (int)anchoBola, (int)altoBola);
+			x = x + vy;
+			y = y - vx;
+			repaint();
+			
+			if(y < 0 ){
+				anchoBola = (int)(Math.random()*130);
+				altoBola = (int)(Math.random()*130);
+				vy = -0.5f;
+				vx = -0.2f;
+				c = Color.RED;
+			}
+			
+			if(y > 870 - altoBola){
+				anchoBola = (int)(Math.random()*160);
+				altoBola = (int)(Math.random()*160);
+				vy = 0.5f;
+				vx = 0.2f;
+				c = Color.GREEN;
+			}
+			if(x > 1830 - anchoBola/2){
+				anchoBola = (int)(Math.random()*190);
+				altoBola = (int)(Math.random()*190);
+				vy = -0.5f;
+				vx = 0.2f;
+				c = Color.YELLOW;
+			}
+			if(x < 0){
+				anchoBola = (int)(Math.random()*200);
+				altoBola = (int)(Math.random()*200);
+				vy = 0.5f;
+				vx = -0.2f;
+				c = Color.CYAN;
+			}
+			
+		
+		
+		
 	}
 }
